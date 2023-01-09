8d3976ea5100b80829c4f753eefa5c51a578b637
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2023-01-09 10:31:02 +0300

    added search client component

--- app/search/Search.tsx
+++ app/search/Search.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
 
 function Search() {
-  return <div>Searching here</div>;
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch("");
+    router.push(`/search/${search}`);
+  };
+  return (
+    <form onSubmit={handleSearch}>
+      <input
+        type="text"
+        value={search}
+        placeholder="Enter the Search term"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="p-2 text-sm bg-blue-500 text-white focus:ring-2 focus:ring-blue-500 ring-offset-1 rounded-md"
+      >
+        Search
+      </button>
+    </form>
+  );
 }
 
 export default Search;
