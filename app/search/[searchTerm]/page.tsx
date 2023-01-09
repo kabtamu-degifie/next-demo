3b41b4a9dce68f04fcda96848b58f796836cfea4
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2023-01-09 11:09:36 +0300

    added loading and error

--- app/search/[searchTerm]/page.tsx
+++ app/search/[searchTerm]/page.tsx
@@ -4,6 +4,7 @@ const search = async (searchTerm: string) => {
   const res = await fetch(
     `https://serpapi.com/search.json?q=${searchTerm}&api_key=${process.env.API_KEY}`
   );
+  // throw new Error("whoops...");
   const data: SearchResult = await res.json();
   return data;
 };
